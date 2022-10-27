0bef54073d6d363cc46b927438a6292a58e7aa18
Ksenia Stupina <k.stupina@okenit.ru>
2022-10-27 14:50:16 +0300

    small fix

--- scripts/throttle.js
+++ scripts/throttle.js
@@ -1,28 +1,28 @@
-function throttle(func, ms){
-    let isThrottling = false;
-    let lastArgs;
-    let lastThis;
-
-    function wrapper(){
-        if(isThrottling){
-            lastArgs = arguments;
-            lastThis = this;
-            return;
-        }
-        func.applay(this, arguments);
-        isThrottling = true;  
-
-        setTimeout(() => {
-            isThrottling = false;
-            if(lastArgs){
-                wrapper.apply(lastArgs, lastThis);
-                lastArgs = null;
-                lastThis = null;
-            }
-        }, ms)
-    }
-    return wrapper;
-}
+// function throttle(func, ms){
+//     let isThrottling = false;
+//     let lastArgs;
+//     let lastThis;
+//
+//     function wrapper(){
+//         if(isThrottling){
+//             lastArgs = arguments;
+//             lastThis = this;
+//             return;
+//         }
+//         func.apply(this, arguments);
+//         isThrottling = true;
+//
+//         setTimeout(() => {
+//             isThrottling = false;
+//             if(lastArgs){
+//                 wrapper.apply(lastArgs, lastThis);
+//                 lastArgs = null;
+//                 lastThis = null;
+//             }
+//         }, ms)
+//     }
+//     return wrapper;
+// }
 
 
 function throttle(f, wait){
@@ -45,7 +45,7 @@ function throttle(f, wait){
                 hasTrailingCall = false;
             }
         }, wait);
-    };
+    }
 
     function wrapper (...args){
         if(!isThrottling){
